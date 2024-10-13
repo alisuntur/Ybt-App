266b273f3ebd536ba4f056f12367c860f2891524
Ali Suntur <sunturali07@gmail.com>
2024-10-13 11:02:48 +0300

    Sorular ve tema konularında geliştirmeler yapıldı

--- ybt_app/lib/question_screen.dart
+++ ybt_app/lib/question_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart'; // Dosya işlemleri için
 import 'dart:io';
+import 'dart:math';
 
 class SorularScreen extends StatefulWidget {
   final String soruTuru;
@@ -16,27 +17,34 @@ class SorularScreen extends StatefulWidget {
 }
 
 class _SorularScreenState extends State<SorularScreen> {
-  late Future<List<Question>> _questions;
+  late Future<List<Question>> _questionsFuture;
+  List<Question> _questions = []; // Tüm soruları tutacak liste
   int _currentQuestionIndex = 0; // Geçerli soru indeksi
   String? _selectedAnswer; // Seçilen cevabı tutar
   int _score = 0; // Toplam puanı tutar
-  Color _backgroundColor = Colors.white; // Arka plan rengi
+  Color _backgroundColor = Colors.blue; // Arka plan rengi
   String? _correctAnswer; // Doğru cevabı tutar
 
   @override
   void initState() {
     super.initState();
-    _questions = loadQuestions();
+    _questionsFuture = loadQuestions();
   }
 
   Future<List<Question>> loadQuestions() async {
     final String response = await rootBundle.loadString('assets/data.json');
     final List<dynamic> data = json.decode(response);
 
-    return data
+    // Sadece seçilen soru türüne göre filtrele
+    List<Question> questions = data
         .where((item) => item['soruTuru'] == widget.soruTuru)
         .map((item) => Question.fromJson(item))
         .toList();
+
+    // Soruları rastgele karıştır
+    questions.shuffle(Random());
+
+    return questions;
   }
 
   void checkAnswer(Question question) {
@@ -55,7 +63,7 @@ class _SorularScreenState extends State<SorularScreen> {
       Future.delayed(const Duration(seconds: 1), () {
         if (mounted) {
           setState(() {
-            _backgroundColor = Colors.white; // Arka plan rengini sıfırla
+            _backgroundColor = Colors.blue; // Arka plan rengini sıfırla
             _currentQuestionIndex++;
             _selectedAnswer = null;
             _correctAnswer = null; // Doğru cevabı sıfırla
@@ -85,15 +93,13 @@ class _SorularScreenState extends State<SorularScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final screenWidth = MediaQuery.of(context).size.width;
-
     return Scaffold(
       appBar: AppBar(
         title: Text(
             '${widget.soruTuru == "1" ? "Genel Kültür" : "Bilişim"} Soruları'),
       ),
       body: FutureBuilder<List<Question>>(
-        future: _questions,
+        future: _questionsFuture, // Future'ı burada kullanıyoruz
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
             return const Center(child: CircularProgressIndicator());
@@ -103,9 +109,10 @@ class _SorularScreenState extends State<SorularScreen> {
             return const Center(child: Text('Hiç soru bulunamadı.'));
           }
 
-          final questions = snapshot.data!;
+          // Burada snapshot.data ile sorulara erişiyoruz
+          _questions = snapshot.data!;
 
-          if (_currentQuestionIndex >= questions.length) {
+          if (_currentQuestionIndex >= _questions.length) {
             saveScore(); // Puanı kaydet
             return Center(
               child: Text(
@@ -117,7 +124,7 @@ class _SorularScreenState extends State<SorularScreen> {
             );
           }
 
-          final question = questions[_currentQuestionIndex];
+          final question = _questions[_currentQuestionIndex];
 
           return AnimatedContainer(
             duration: const Duration(milliseconds: 500),
@@ -130,55 +137,77 @@ class _SorularScreenState extends State<SorularScreen> {
                   Padding(
                     padding: const EdgeInsets.all(16.0),
                     child: Text(
-                      question.soru,
+                      'Soru ${_currentQuestionIndex + 1}: ${question.soru}', // İndeksi burada göster
                       style: const TextStyle(
                           fontSize: 18, fontWeight: FontWeight.bold),
                       textAlign: TextAlign.center,
                     ),
                   ),
                   Column(
-                    children: question.cevaplar.map((cevap) {
-                      // Doğru cevabı belirle
-                      bool isCorrectAnswer = cevap ==
-                          question.cevaplar[int.parse(question.dogruCevap) - 1];
-                      bool isSelectedAnswer = _selectedAnswer == cevap;
-
-                      return Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 8.0),
-                        child: SizedBox(
-                          width: screenWidth * 0.9,
-                          child: ElevatedButton(
-                            onPressed: () {
-                              setState(() {
-                                _selectedAnswer = cevap;
-                              });
-                              checkAnswer(question); // Cevabı kontrol et
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: isSelectedAnswer
-                                  ? (isCorrectAnswer
-                                      ? Colors.green // Doğru seçenek
-                                      : Colors.red) // Yanlış seçilen seçenek
-                                  : (isCorrectAnswer && _selectedAnswer != null
-                                      ? Colors
-                                          .yellow // Yanlış seçilen seçenek varsa doğru seçenek sarı
-                                      : Colors.grey), // Seçenek seçilmedi
-                            ),
-                            child: Text(
-                              cevap,
-                              textAlign: TextAlign.center,
-                              style: const TextStyle(fontSize: 16),
-                            ),
-                          ),
-                        ),
-                      );
-                    }).toList(),
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          _buildAnswerButton(question.cevaplar[0], question),
+                          const SizedBox(
+                              width: 16), // İki buton arasında boşluk
+                          _buildAnswerButton(question.cevaplar[1], question),
+                        ],
+                      ),
+                      const SizedBox(height: 16), // Satırlar arası boşluk
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          _buildAnswerButton(question.cevaplar[2], question),
+                          const SizedBox(
+                              width: 16), // İki buton arasında boşluk
+                          _buildAnswerButton(question.cevaplar[3], question),
+                        ],
+                      ),
+                    ],
                   ),
                 ],
               ),
             ),
           );
         },
+      ),
+    );
+  }
+
+  // Cevap butonunu oluşturan fonksiyon
+  Widget _buildAnswerButton(String cevap, Question question) {
+    bool isCorrectAnswer =
+        cevap == question.cevaplar[int.parse(question.dogruCevap) - 1];
+    bool isSelectedAnswer = _selectedAnswer == cevap;
+
+    // Buton rengini ayarlama
+    Color buttonColor;
+    if (isSelectedAnswer) {
+      buttonColor = isCorrectAnswer
+          ? Colors.green
+          : Colors.red; // Doğru cevap yeşil, yanlış cevap kırmızı
+    } else if (isCorrectAnswer && _correctAnswer != null) {
+      buttonColor = Colors.yellow; // Doğru cevap, ama seçilmediğinde sarı
+    } else {
+      buttonColor = Colors.teal[100]!; // Normal durum
+    }
+
+    return ElevatedButton(
+      onPressed: () {
+        setState(() {
+          _selectedAnswer = cevap;
+        });
+        checkAnswer(question); // Cevabı kontrol et
+      },
+      style: ElevatedButton.styleFrom(
+        backgroundColor: buttonColor,
+        minimumSize: const Size(200, 150), // Buton boyutu
+      ),
+      child: Text(
+        cevap,
+        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        textAlign: TextAlign.center,
       ),
     );
   }
